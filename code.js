12473bcc1d6fa83f0af82068b7457ac5788dc8f6
vincode-vinwin <vincentalexanderschultz@gmx.de>
2026-04-01 14:13:36 +0200

    added cactus

--- code.js
+++ code.js
@@ -63,11 +63,46 @@ window.onload = function() {
 
 
   requestAnimationFrame(update);
+  setInterval(placeCactus, 1000); // 1000 miliseconds = 1 second
 
 }
 
 function update() {
   requestAnimationFrame(update);
-
+  
+  // dino:
   context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+
+  // cactus:
+  for (let i = 0; i < cactusArray.length; i++) {
+    let cactus = cactusArray[i];
+    context.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height);
+  }
+}
+
+function placeCactus() {
+  // place cactus
+  let cactus = {
+    img : null,
+    x : cactusX,
+    y : cactusY,
+    width : null,
+    height : cactusHeight
+  }
+
+  whichCactus = Math.random(); // 0 - 0.999999999......
+
+  if (whichCactus > .90) { // 10% you get cactus3
+    cactus.img = cactus3Img;
+    cactus.width = cactus3Width;
+    cactusArray.push(cactus);
+  } else if (whichCactus > .70) { // 20% you get cactus2
+    cactus.img = cactus2Img;
+    cactus.width = cactus2Width;
+    cactusArray.push(cactus);
+  } else if (whichCactus > .40) { // 30% you get cactus1
+    cactus.img = cactus1Img;
+    cactus.width = cactus1Width;
+    cactusArray.push(cactus);
+  }
 }
